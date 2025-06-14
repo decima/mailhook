2abfed4cac5b7b55ec18adc5ce15e669f53dfbf4
decima <h.larget@gmail.com>
2025-06-14 14:35:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+Mailhook
+================
+
+A simple mailhook server that listens for incoming emails and transform them into a webhook request.
+
+## Configure
+
+You can configure multiple webhook by using the following config example.
+You can specify multiple from/to, and the action associated with these from/to.
+you can add multiple actions for the same from/to combination.
+
+```jsonc
+[
+  {
+    "from": [
+      "decima@zeus"
+    ],
+    "to": [
+      "do@localhost"
+    ],
+    "action": {
+      "url": "http://localhost:18000/00000000-0000-0000-0000-000000000000",
+      "method": "POST",
+      "format": "json",
+      "body": {
+        "subject": "{{subject}}",
+        "from": "{{from}}",
+        "to": "{{to}}",
+        "text": "{{text}}",
+        "some": "other",
+        "json": "data"
+      }
+    }
+  }
+]
+```
+
+
+## How to use
+
+With docker-compose: 
+```yaml
+services:
+  mailhook:
+    image: decima/mailhook:0.1.0
+    ports:
+      - "25:2525"
+    volumes:
+      - ./config.json:/app/config.json
+```
+
+## Development and local testing purpose
+For this to run, you'll need:
+- Docker (with compose, but let's be honest, if you don't have docker compose, it may be time to upgrade your stack)
+- nix (optional, but recommended for development)
+- swaks (a tool to send emails, useful for testing)
+
+run
+```shell
+make local.start.deps
+```
+to start local deps. (not mandatory, but useful for development)
+then you can go to [localhost:18000/s/00000000-0000-0000-0000-000000000000](http://localhost:18000/s/00000000-0000-0000-000000000000) to see the webhook endpoint.
+
+Then you can run ```make local.run``` which will start the server on port 2525.
+
+if you want to test you can use : 
+```shell
+make test.mail
+make test.mail.full
+```
+
+
+### Using nix
+you can run
+```shell
+make
+```
+and it will start deps and install swaks.
---
+++ config/ConfigManager.go
@@ -0,0 +1,174 @@
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type Email struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Text    string `json:"text"`
+}
+
+func (e *Email) ApplyPlaceholders(str string) string {
+	placeHolders := map[string]string{
+		"from":    e.From,
+		"to":      e.To,
+		"subject": e.Subject,
+		"text":    e.Text,
+	}
+	for key, value := range placeHolders {
+		str = strings.Replace(str, "{{"+key+"}}", value, -1)
+	}
+	return str
+}
+
+type Handler struct {
+	From   []string `json:"from"`
+	To     []string `json:"to"`
+	Action Action   `json:"action"`
+}
+
+type Action struct {
+	URL    string `json:"url"`
+	Method string `json:"method"`
+	Format string `json:"format"`
+	Body   any    `json:"body,omitempty"`
+}
+
+func (a *Action) Run(e Email) error {
+	httpClient := &http.Client{}
+
+	req, err := http.NewRequest(a.Method, a.URL, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %w", err)
+	}
+
+	if a.Body != nil {
+		var content string = ""
+
+		switch a.Format {
+		case "json":
+			jsonBody, err := json.Marshal(a.Body)
+			if err != nil {
+				return fmt.Errorf("error marshalling body to JSON: %w", err)
+			}
+			content = string(jsonBody)
+			req.Header.Set("Content-Type", "application/json")
+		default:
+			return fmt.Errorf("unsupported format: %s", a.Format)
+
+		}
+
+		tmp := e.ApplyPlaceholders(content)
+
+		req.Body = io.NopCloser(bytes.NewBufferString(tmp))
+
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+type Allowed struct {
+	Emails []string
+}
+
+func (allowed *Allowed) Add(email string) {
+	if email != "" && !allowed.Has(email) {
+		allowed.Emails = append(allowed.Emails, email)
+	}
+}
+
+func (allowed *Allowed) Has(email string) bool {
+	for _, e := range allowed.Emails {
+		if e == email {
+			return true
+		}
+	}
+	return false
+}
+
+func (allowed *Allowed) IsAllowed(email string) bool {
+	for _, e := range allowed.Emails {
+		if e == email {
+			return true
+		}
+	}
+	return false
+}
+
+var Recipients = Allowed{}
+var Senders = Allowed{}
+
+func LoadConfig() error {
+	var cfg []Handler
+	fileName := "config.json"
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return fmt.Errorf("error reading config file %s: %w", fileName, err)
+	}
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return fmt.Errorf("error parsing config file %s: %w", fileName, err)
+	}
+	log.Printf("Loaded config")
+
+	for _, h := range cfg {
+		for _, a := range h.From {
+			Senders.Add(a)
+		}
+		for _, a := range h.To {
+			Recipients.Add(a)
+		}
+		for _, from := range h.From {
+			for _, to := range h.To {
+				fromToPair := uniquePair(from, to)
+				Config[fromToPair] = append(Config[fromToPair], h.Action)
+			}
+		}
+	}
+
+	return nil
+}
+
+var Config = map[string][]Action{}
+
+func uniquePair(from string, to string) string {
+	return fmt.Sprintf("%s->%s", from, to)
+}
+
+func IsAllowedEmail(email string) bool {
+	return Recipients.IsAllowed(email) || Senders.IsAllowed(email)
+}
+func RunActions(from string, to string, subject string, text string) error {
+	email := Email{
+		From:    from,
+		To:      to,
+		Subject: subject,
+		Text:    text,
+	}
+	fromToPair := uniquePair(from, to)
+	actions, exists := Config[fromToPair]
+	if !exists {
+		return nil
+	}
+	for _, action := range actions {
+		if err := action.Run(email); err != nil {
+			return err
+		}
+	}
+	return nil
+}
